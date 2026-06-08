10610bfbd585f6078a8592fe2a096ac439dc7bb4
enum-villager <mirror@enum.co>
2026-06-08 07:11:21 +0000

    mirror: sync from a1f9bff

---
+++ README.md
@@ -0,0 +1,72 @@
+# enum Go API Client
+
+Go client for the [enum](https://enum.co) API.
+
+## Install
+
+```bash
+go get github.com/enumco/client-go
+```
+
+## Usage
+
+```go
+package main
+
+import (
+ "context"
+ "fmt"
+ "log"
+
+ client "github.com/enumco/client-go"
+ apiv1 "github.com/enumco/proto-gen-go/gen/enum/api/v1"
+)
+
+func main() {
+ c, err := client.New(client.WithToken("your-api-token"))
+ if err != nil {
+  log.Fatal(err)
+ }
+ defer c.Close()
+
+ resp, err := c.Organizations.ListOrganizations(context.Background(), &apiv1.ListOrganizationsRequest{})
+ if err != nil {
+  log.Fatal(err)
+ }
+
+ for _, org := range resp.GetOrganizations() {
+  fmt.Println(org.GetName())
+ }
+}
+```
+
+## Services
+
+| Field | Service |
+|---|---|
+| `c.Organizations` | OrganizationService |
+| `c.Projects` | ProjectService |
+| `c.Users` | UserService |
+| `c.Kubernetes.Clusters` | KubernetesClusterService |
+| `c.ObjectStorage.Users` | ObjectStorageUserService |
+| `c.ObjectStorage.AccessKeys` | ObjectStorageAccessKeyService |
+| `c.ObjectStorage.Buckets` | ObjectStorageBucketService |
+
+## Options
+
+| Option | Description |
+|---|---|
+| `WithToken(token string)` | Bearer token for authentication |
+| `WithAddr(addr string)` | Override the API endpoint (default: `api.enum.co:443`) |
+| `WithTLS(creds credentials.TransportCredentials)` | Custom TLS configuration |
+| `WithInsecure()` | Disable TLS - for local development only |
+
+The client uses TLS with system roots by default. No extra configuration needed for production use.
+
+## Documentation
+
+See [enum Docs](https://docs.enum.co/api/).
+
+## License
+
+[Apache License 2.0](LICENSE)
---
+++ client.go
@@ -0,0 +1,71 @@
+package client
+
+import (
+	"crypto/tls"
+	"slices"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	apiv1 "github.com/enumco/proto-gen-go/gen/enum/api/v1"
+)
+
+const defaultAddr = "api.enum.co:443"
+
+type KubernetesClient struct {
+	Clusters apiv1.KubernetesClusterServiceClient
+}
+
+type ObjectStorageClient struct {
+	Users      apiv1.ObjectStorageUserServiceClient
+	AccessKeys apiv1.ObjectStorageAccessKeyServiceClient
+	Buckets    apiv1.ObjectStorageBucketServiceClient
+}
+
+type Client struct {
+	Organizations apiv1.OrganizationServiceClient
+	Projects      apiv1.ProjectServiceClient
+	Users         apiv1.UserServiceClient
+	Kubernetes    KubernetesClient
+	ObjectStorage ObjectStorageClient
+
+	conn *grpc.ClientConn
+}
+
+// c, err := client.New(client.WithToken("..."))
+func New(opts ...ClientOption) (*Client, error) {
+	o := &options{addr: defaultAddr}
+	for _, opt := range opts {
+		opt(o)
+	}
+
+	if o.creds == nil {
+		o.creds = credentials.NewTLS(&tls.Config{})
+	}
+
+	dialOpts := append(slices.Clone(o.dialOpts), grpc.WithTransportCredentials(o.creds))
+
+	conn, err := grpc.NewClient(o.addr, dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		Organizations: apiv1.NewOrganizationServiceClient(conn),
+		Projects:      apiv1.NewProjectServiceClient(conn),
+		Users:         apiv1.NewUserServiceClient(conn),
+		Kubernetes: KubernetesClient{
+			Clusters: apiv1.NewKubernetesClusterServiceClient(conn),
+		},
+		ObjectStorage: ObjectStorageClient{
+			Users:      apiv1.NewObjectStorageUserServiceClient(conn),
+			AccessKeys: apiv1.NewObjectStorageAccessKeyServiceClient(conn),
+			Buckets:    apiv1.NewObjectStorageBucketServiceClient(conn),
+		},
+		conn: conn,
+	}, nil
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close()
+}
